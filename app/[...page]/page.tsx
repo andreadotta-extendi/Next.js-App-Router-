d7b92102e0f0b208346c407149f654c52cd190f3
andreadotta-extendi <dotta@extendi.it>
2024-08-01 10:20:36 +0200

    fix builder

--- app/[...page]/page.tsx
+++ app/[...page]/page.tsx
@@ -1,5 +1,6 @@
 import { builder } from "@builder.io/sdk";
 import { RenderBuilderContent } from "../../components/builder";
+import React from 'react';
 
 // Builder Public API Key set in .env file
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 
-import Login from '@/components/Login';
-import PricingPage from '@/components/PricingPage';
-import TeachersPage from '@/components/TeachersPage';
 
 export default function Home() {
   return (
     <main>
-      <TeachersPage />
     
     </main>
   );
